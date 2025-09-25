7355104b1365ca11cecdb188da16b3865dadbf73
OmkarNarkar321 <159636724+OmkarNarkar321@users.noreply.github.com>
2025-09-26 00:23:27 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Assignment1 from "./Assignment1/assignment1.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <Assignment1 />
+    </div>
+  );
+}
